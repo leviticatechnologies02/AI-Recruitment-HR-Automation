3eb9bc34b780c65cb3771a2bc6164828269bdd3a
nagendrauggirala <146092953+NagendraUggirala@users.noreply.github.com>
2025-09-05 13:01:25 +0530

    nag2

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Route, Routes} from "react-router-dom";
-import Home from './components/dashboard/Home'
+
 import Login from './components/Login'
 import Signup from './components/Signup'
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = () => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />  
     </Routes>
---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Sidebars from './Sidebars'
+
+const Dashboard = () => {
+  return (
+    <div>
+
+      <Sidebars/>
+    </div>
+
+  )
+}
+
+export default Dashboard
