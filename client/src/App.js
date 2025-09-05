8cc56f4fbf7afe75a01977478f46635c3890477a
nagendrauggirala <146092953+NagendraUggirala@users.noreply.github.com>
2025-09-05 11:31:27 +0530

    nag1

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Route, Routes} from "react-router-dom";
+import Home from './components/dashboard/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />  
+    </Routes>
+    </>
+  )
+}
+
+export default App
